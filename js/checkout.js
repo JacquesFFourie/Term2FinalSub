9a10ce5b1442f999b142b182ca23986ae113e5c5
JacquesFFourie <125289955+JacquesFFourie@users.noreply.github.com>
2023-06-18 22:54:43 +0200

    Final Comit

--- js/checkout.js
+++ js/checkout.js
@@ -5,18 +5,38 @@ showOrder = () =>{
 
     console.log(data)
     dataName = data[0].subName;
-    dataSize = data[0].subSize;
+    dataBread = data[0].subBread;
     dataPrice = data[0].price;
     dataToppingsOne = data[0].subToppingOne;
     dataToppingsTwo = data[0].subToppingTwo;
     dataToppingsThree = data[0].subToppingThree;
+    dataToppingsFour = data[0].subToppingFour;
+    dataToppingsFive = data[0].subToppingFive;
+    dataToppingsSix = data[0].subToppingSix;
+    dataSauce = data[0].subSauce;
 
 
     document.getElementById("subNameCheck").innerHTML = "Name of sub: " + dataName;
-    document.getElementById("subSizeCheck").innerHTML = "Size of sub: " + dataSize;
+    document.getElementById("subBreadCheck").innerHTML = "Size of sub: " + dataBread;
     document.getElementById("subPriceCheck").innerHTML = "Price: R" + dataPrice + ".00";
     document.getElementById("subPriceCheckFinal").innerHTML = "R" + dataPrice + ".00";
-    document.getElementById("subToppings").innerHTML = "Toppings " + dataToppingsOne + " " + dataToppingsTwo + " " + dataToppingsThree;
+    document.getElementById("subToppings").innerHTML = "Toppings " + dataToppingsOne + ", " + dataToppingsTwo + ", " + dataToppingsThree + ", " + dataToppingsFour + ", " + dataToppingsFive + " and " + dataToppingsSix;
+    document.getElementById("subSauceName").innerHTML = "Sauce: " + dataSauce;
+}
+
+discountCode = () =>{
+    let discountCode = document.getElementById("discountCode").value;
+    let dataCheckout = JSON.parse(localStorage.getItem('order'))
+    priceData = dataCheckout[0].price;
+
+    if(discountCode == 1111){
+         
+        priceData = priceData - (priceData*30/100)
+
+        document.getElementById("subPriceCheckFinal").innerHTML = "R" + priceData + ".00"
+        document.getElementById("subPriceCheck").innerHTML = "R" + priceData + ".00"
+        document.getElementById("DiscountText").innerHTML = "30% off! LETS GO!!"
+    }
 }
 
 resetReturn = () => {
--- js/index.js
+++ js/index.js
@@ -4,43 +4,51 @@ let subOrder = [];
 makeSub = () =>{
 
     let subName = document.getElementById("subName").value;
-    let subSize = document.getElementById("subSize").value;
+    let subBread = document.getElementById("subBread").value;
     let subToppingOne = document.getElementById("subToppingOne").value;
     let subToppingTwo = document.getElementById("subToppingTwo").value;
     let subToppingThree = document.getElementById("subToppingThree").value;
+    let subToppingFour = document.getElementById("subToppingFour").value;
+    let subToppingFive = document.getElementById("subToppingFive").value;
+    let subToppingSix = document.getElementById("subToppingSix").value;
+    let subSauce = document.getElementById("subSauce").value;
     
 
 
 
 
 
-    let price = 0;
+    let price = 50;
 
-    if(subSize === "Small"){
+    if(subBread === "Plain"){
         price = price + 10;
-    }else if(subSize === "Medium"){
+    }else if(subBread === "Lightly Toasted"){
         price = price + 20;
-    }else if(subSize === "Large"){
+    }else if(subBread === "TOASTED"){
         price = price + 30;
     }
 
     subOrder.push({
         subName: subName,
-        subSize: subSize,
+        subBread: subBread,
         price: price,
         subToppingOne: subToppingOne,
         subToppingTwo: subToppingTwo,
-        subToppingThree: subToppingThree
+        subToppingThree: subToppingThree,
+        subToppingFour: subToppingFour,
+        subToppingFive: subToppingFive,
+        subToppingSix: subToppingSix,
+        subSauce: subSauce
     });
     
 
     console.log(subOrder);
     
     document.getElementById("subNaming").innerHTML = "Name of sub: " + subName;
-    document.getElementById("subSizing").innerHTML = "Size: " + subSize;
+    document.getElementById("subSizing").innerHTML = "Size: " + subBread;
     document.getElementById("totalCost").innerHTML = "R " + price;
-    document.getElementById("subToppings").innerHTML = "Toppings: " + " " +subToppingOne+ " " + subToppingTwo+ " " + subToppingThree;
-
+    document.getElementById("subToppings").innerHTML = "Toppings: " + " " +subToppingOne+ " " + subToppingTwo+ " " + subToppingThree + " " + subToppingFour+ " " + subToppingFive+ " " + subToppingSix;
+    document.getElementById("subSauceName").innerHTML = "Sauce: " + subSauce;
 }
 
 checkout = () => {
